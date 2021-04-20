1364c4fce3e96f0e73a232c0e09bb27a98be29a6
octavius2929 <justinoctavio2001@gmail.com>
2021-04-20 14:33:06 -0400

    pro env added

--- config/env/pro.ts
+++ config/env/pro.ts
@@ -1,3 +1,20 @@
-const pro: any = {};
+import * as dotnet from 'dotenv';
+dotnet.config();
 
+const pro = {
+  PORT: process.env.PORT || 80,
+  JWT_SECRET: process.env.SECRET,
+  db: {
+    HOST: process.env.DATABASE_HOST,
+    DATABASE: process.env.DATABASE_NAME,
+    PORT: process.env.DATABASE_PORT || '5432',
+    PASSWORD: process.env.DATABASE_PASSWORD,
+    USER: process.env.DATABASE_USERNAME,
+  },
+  admin: {
+    USERNAME: process.env.ADMIN_USERNAME,
+    PASSWORD: process.env.ADMIN_PASSWORD,
+    EMAIL: process.env.ADMIN_EMAIL,
+  },
+};
 export default pro;
